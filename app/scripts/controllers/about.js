2362fbee9b66b7c8f28aa04cb58d24878b72cfaf
pajamaw <pjpeterww@gmail.com>
2016-05-12 20:23:40 -0400

    Added yo framework and rails api

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name suitematesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the suitematesApp
+ */
+angular.module('suitematesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name suitematesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the suitematesApp
+ */
+angular.module('suitematesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
